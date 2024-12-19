6193d65d70e36da50e871b70ba258d45ed2d0f8d
ADNAN BAVA <94239007+ADNAN-BAVA@users.noreply.github.com>
2024-12-19 06:49:35 +0400

    Update exception.py

--- src/exception.py
+++ src/exception.py
@@ -13,7 +13,7 @@ def error_message_detail(error,error_detail:sys):
 
 class CustomException(Exception):
     def __init__(self,error_message:str,error_detail:sys):
-        super().__init__(error_message) # call the parent class constructor
+        super().__init__(error_message) # calling the parent class constructor 
         self.error_message = error_message_detail(error_message,error_detail=error_detail) # get the error message
     
     def __str__(self):
